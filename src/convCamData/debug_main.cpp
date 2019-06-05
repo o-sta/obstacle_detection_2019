07aa0a2f40d91f766d7b93fb71275167d4a6eda2
o-sta <41351962+o-sta@users.noreply.github.com>
2019-06-05 12:40:46 +0900

    Completed

--- src/convCamData/debug_main.cpp
+++ src/convCamData/debug_main.cpp
@@ -6,18 +6,18 @@ int main(int argc,char **argv){
 	ros::init(argc,argv,"obstacle_detection_2019_rg");
 	
     convCamDataClass ccd; //pan_tilt_control_instance
-	std::cout << "convCamDataClass instance generated.\n"; 
+	ROS_INFO_STREAM("convCamDataClass instance generated.\n");
 	ros::Rate loop_rate(60);
 	ros::Time ransac_time = ros::Time::now();
 	while (ros::ok()) {
 		ccd.subscribeSensorData();
 		if (ros::Time::now() - ransac_time > ros::Duration(1)){
 			if (ccd.is_SensorData_subscribed()){
-				std::cout << "RANSAC start.\n";
+				ROS_INFO_STREAM("RANSAC start.\n");
 				ccd.groundEstimationRANSAC();
 				ransac_time = ros::Time::now();
 			}else{
-				std::cout << "Sensor data not exist.\n";
+				ROS_INFO_STREAM("Sensor data not exist.\n");
 			}
 		}
 		loop_rate.sleep();
--- src/convCamData/debug_main_genmsg.cpp
+++ src/convCamData/debug_main_genmsg.cpp
@@ -2,26 +2,23 @@
 #include <obstacle_detection_2019/convCamData.h>
 #include <iostream>
 
+
+//このプログラムは受け取ったデータを外部に送信します。
 int main(int argc,char **argv){
 	ros::init(argc,argv,"obstacle_detection_2019_rg");
-	
-    convCamDataClass ccd; //pan_tilt_control_instance
-	std::cout << "convCamDataClass instance generated.\n"; 
-	ros::Rate loop_rate(60);
+	int lr_value = 60; //loop rate value
+	ros::param::param("loop_rate", lr_value, lr_value);
+    convCamDataClass ccd;
+	//ROS_INFO_STREAM("convCamDataClass instance generated.\n");
+	ros::Rate loop_rate(lr_value);
 	ros::Time ransac_time = ros::Time::now();
 	while (ros::ok()) {
-		ccd.subscribeSensorData();
-		if (ros::Time::now() - ransac_time > ros::Duration(1)){
-			if (ccd.is_SensorData_subscribed()){
-				std::cout << "RANSAC start.\n";
-				ccd.groundEstimationRANSAC();
-				ransac_time = ros::Time::now();
-				ccd.createPubDataRANSAC();
-				ccd.publishConvCamData();
-				ccd.publishMaskImage();
-			}else{
-				std::cout << "Sensor data not exist.\n";
-			}
+		if(ccd.subscribeSensorData()){
+			//ROS_INFO_STREAM("RANSAC start");
+			ccd.groundEstimationRANSAC();
+			ccd.createPubDataRANSAC();
+			ccd.publishConvCamData();
+			ccd.publishMaskImage();
 		}
 		loop_rate.sleep();
 	}
