b8ee76a84484a282b802247763dc736ae2982509
o-sta <41351962+o-sta@users.noreply.github.com>
2019-10-23 13:39:54 +0900

    develop

--- src/darknetImg/debug/methods.cpp
+++ src/darknetImg/debug/methods.cpp
@@ -46,15 +46,15 @@ void darknetImgDebug::debug_callback(const darknet_ros_msgs::BoundingBoxes::Cons
         trimPoints(boundingBoxesMsg);
         ROS_INFO_STREAM("generateGridmap");
         generateGridmap();
-        ROS_INFO_STREAM("dimensionalityReductionGridmap");
-        dimensionalityReductionGridmap();
-        ROS_INFO_STREAM("classifyPoints");
-        classifyPoints();
-        ROS_INFO_STREAM("estimatePersonPosition");
-        estimatePersonPosition();
-        ROS_INFO_STREAM("predictPersonPosition");
-        predictPersonPosition();
-        ROS_INFO_STREAM("iteration finished");
+        // ROS_INFO_STREAM("dimensionalityReductionGridmap");
+        // dimensionalityReductionGridmap();
+        // ROS_INFO_STREAM("classifyPoints");
+        // classifyPoints();
+        // ROS_INFO_STREAM("estimatePersonPosition");
+        // estimatePersonPosition();
+        // ROS_INFO_STREAM("predictPersonPosition");
+        // predictPersonPosition();
+        // ROS_INFO_STREAM("iteration finished");
     }else{
         ROS_INFO_STREAM("not person detection");
     }
